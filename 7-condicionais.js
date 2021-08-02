9500326a55f49f6af1af198faf3922fc4f9c2e21
Vinicius <86333257+viniflopes@users.noreply.github.com>
2021-08-02 14:58:02 -0300

    Aplicação de if/else concatenados

--- 7-condicionais.js
+++ 7-condicionais.js
@@ -5,19 +5,26 @@ const listaDeDestinos = new Array(
     `São Paulo`,
     `Rio de Janeiro`
 )
-const idadeComprador = 14; // Alterando a const idadeComprador podemos alterar a permissão do if abaixo
-const estaAcompanhada = true; // true sinaliza acompanhado, false desacompanhado, dando condição ou não à condição de comprar passagem
+const idadeComprador = 18; // Alterando a const idadeComprador podemos alterar a permissão do if abaixo
+const estaAcompanhada = false; // true sinaliza acompanhado, false desacompanhado, dando condição ou não à condição de comprar passagem
+const temPassagemComprada = true;
+const foiEducado = true;
+
 console.log("Destinos Possíveis");
 console.log(listaDeDestinos);
 
-if (idadeComprador >= 18) {
-    console.log("Comprador maior de idade");
-    listaDeDestinos.splice(1, 1)
-} else if (estaAcompanhada) {
-        console.log("Comprador acompanhado");
-        listaDeDestinos.splice(1, 1)
+if (idadeComprador >= 18 || estaAcompanhada == true) {
+    console.log("Boa Viagem!");
+    listaDeDestinos.splice(2, 1)
     } else {
         console.log("Menor de idade não pode realizar compra sozinho");
     }
+    console.log("Embarque: \n\n")
+
+if(idadeComprador >= 18 && temPassagemComprada && foiEducado) {
+        console.log("Boa viagem");
+    } else {
+        console.log("Você não pode embarcar");
+    } 
 
 console.log(listaDeDestinos);
